b7b4c7c7a7e760a27bb0f6feb8fcb750a3630103
leonardoGomes <leonardojpinheirogomes@gmail.com>
2022-06-14 10:42:34 -0300

    atividade javaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = "Leonardo"
+let sobrenome = "Gomes"
+let anoNascimento = 1991
+let cidade = "recife"
+
+console.log( `Meu nome é ${nome} ${sobrenome}, tenho ${2022-anoNascimento} anos e moro em ${cidade}`)
